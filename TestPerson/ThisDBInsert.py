ab4b07fc2826e2205ef98035dd7dd9914e4a3342
jrom876 <jrom876@gmail.com>
2020-04-01 11:27:45 -0600

    Update ThisDBInsert.py

--- TestPerson/ThisDBInsert.py
+++ TestPerson/ThisDBInsert.py
@@ -77,8 +77,8 @@ def displayAllInDatabase():
         print(i)
     connection.close()
 
-def transferGroupIntoTable():
-    gp0 = ThisGroup()
+def transferGroupIntoTable(array):
+    gp0 = ThisGroup(array)
     tfile = gp0.readGroupFromFile()
     connection = sqlite3.connect("myTable.db")
     crsr = connection.cursor()
@@ -200,7 +200,7 @@ def getuserInput():
         connection.commit()
         connection.close()
     elif choice == 5:   # transfer data from text file to database
-        transferGroupIntoTable()
+        transferGroupIntoTable(personSQLArray)
     elif choice == 55:   # transfer data from database to text file, incomplete
         transferTableIntoGroupList()
     elif choice == 555:   # test
